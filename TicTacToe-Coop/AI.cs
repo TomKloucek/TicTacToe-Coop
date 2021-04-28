e5e2c51d1e1e1d8ae020089ca2f1dc369e17681d
Petr <petrjun2@seznam.cz>
2021-04-28 10:20:10 +0200

    Update of AI

---
+++ TicTacToe-Coop/AI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe_Coop
+{
+    class AI
+    {
+        public Hra hra { get; set; }
+
+        public AI(Hra hra)
+        {
+            this.hra = hra;
+        }
+
+        public static void Vypln()
+        {
+            int[,] volnePole;
+            List<int[,]> poleVolnychPoli = new List<int[,]>();
+            Random rnd = new Random();
+            for (int x = 0; x<3; x++)
+            {
+                for (int y = 0; y > -3; y--)
+                {
+                    if (Hra.Obsazeno() == false)
+                    {
+                        volnePole = new int[x, y];
+                        poleVolnychPoli.Add(volnePole);
+                    }
+                }
+            }
+            int index = rnd.Next(poleVolnychPoli.Count);
+            Console.WriteLine(poleVolnychPoli[index]);
+        }
+    }
+}
