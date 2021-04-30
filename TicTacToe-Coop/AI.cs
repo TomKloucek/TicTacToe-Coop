feb6abf9615e42fd64afc4754f8e73e910a15cdb
Petr <petrjun2@seznam.cz>
2021-05-01 00:07:07 +0200

    ai 2 dalsi moznosti vyplnovani

--- TicTacToe-Coop/AI.cs
+++ TicTacToe-Coop/AI.cs
@@ -16,21 +16,92 @@ namespace TicTacToe_Coop
         public bool Hraj()
         {
             bool obsazeno = true;
-            int prvniDohrana = 0;
-            int druhaDohrana = 0;
             int prvni = 0;
             int druha = 0;
             Random rnd = new Random();
             while (obsazeno)
             {
-                prvni = rnd.Next(hra.velikost);
+               /* if (this.hra.Obsazeno(0, 0) == false || this.hra.Obsazeno(prvni, druha) == false)
+                {
+                    prvni = 0;
+                    druha = 1;
+                    obsazeno = false;
+                }
+                else if (this.hra.Obsazeno(1, 0) == false || this.hra.Obsazeno(prvni, druha) == false)
+                {
+                    prvni = 1;
+                    druha = 1;
+                    obsazeno = false;
+                }
+                else if (this.hra.Obsazeno(2, 0) == false || this.hra.Obsazeno(prvni, druha) == false)
+                {
+                    prvni = 2;
+                    druha = 1;
+                    obsazeno = false;
+                }
+                else if (this.hra.Obsazeno(0, 1) == false || this.hra.Obsazeno(prvni, druha) == false)
+                {
+                    prvni = 0;
+                    druha = 2;
+                    obsazeno = false;
+                }
+                else if (this.hra.Obsazeno(1, 1) == false || this.hra.Obsazeno(prvni, druha) == false)
+                {
+                    prvni = 1;
+                    druha = 2;
+                    obsazeno = false;
+                }
+                else if (this.hra.Obsazeno(2, 1) == false || this.hra.Obsazeno(prvni, druha) == false)
+                {
+                    prvni = 2;
+                    druha = 2;
+                    obsazeno = false;
+                }
+                else if (this.hra.Obsazeno(0, 2) == false || this.hra.Obsazeno(prvni, druha) == false)
+                {
+                    prvni = 0;
+                    druha = 0;
+                    obsazeno = false;
+                }
+                else if (this.hra.Obsazeno(1, 2) == false || this.hra.Obsazeno(prvni, druha) == false)
+                {
+                    prvni = 1;
+                    druha = 0;
+                    obsazeno = false;
+                }
+                else if (this.hra.Obsazeno(2, 2) == false || this.hra.Obsazeno(prvni, druha) == false)
+                {
+                    prvni = 2;
+                    druha = 0;
+                    obsazeno = false;
+                }*/
+
+                /*for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++) 
+                    {
+                        prvni = i;
+                        druha = j;
+                        if (this.hra.Obsazeno(prvni, druha) == false)
+                        {
+                            obsazeno = false;
+                            break;
+                        }
+                    }
+                    if (obsazeno == false)
+                    {
+                        break;
+                    }
+                }*/
+
+                /*prvni = rnd.Next(hra.velikost);
                 druha = rnd.Next(hra.velikost);
                 if (this.hra.Obsazeno(prvni, druha) == false)
                 {
                     obsazeno = false;
-                }
-            }
+                }*/
 
+            }
             hra.hraciPole[prvni, druha] = 'O';
             return hra.Konec(prvni, druha,'O');
         }
